6319c1928566bb68ed1bef3bffb2408d4f7b35d1
Michael <anhumimar@gmail.com>
2022-07-15 16:05:05 -0400

    added array and gallery logic to js

---
+++ assets/js/array.js
@@ -0,0 +1,25 @@
+// Declare an array object for our array of images
+let imageArray = [];
+let noImageArray = [];
+
+imageArray.push([
+  "../images/no-photo-icon-22.png",
+  "No Image",
+]);
+
+imageArray.push([
+  "https://site.com/image-2.jpeg",
+  "Title 2",
+  "720px",
+  "1280px",
+]);
+
+imageArray.push([
+  "https://site.com/image-3.jpeg",
+  "Title 3",
+  "1080px",
+  "1920px",
+]);
+
+// Output imageArray to the console
+console.log(imageArray);
---
+++ assets/js/gallery.js
@@ -0,0 +1,5 @@
+var createImgs = function (data) {
+    for (let i = 1; i < data.results.length; i++){
+        let galleryImg = data.results[i].image;
+    }
+}
