5faf0e2dc5cb61981b5884163a4bc0e43bd2ee32
Michael Zelaya <Anhumimar@gmail.com>
2022-07-22 20:32:31 -0400

    fixed array

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,36 +1,4 @@
-// -------------TEST ARRAY 1-------------------------
-// // Declare an array object for our array of images
-// let array = [];
-
-// array.push([
-//   // grandma //
-//   "Julia Abarca Chavez",
-//   [
-//     "../image/no_photo.png",
-//     "../image/no_photo.png",
-//     "../image/no_photo.png",
-//     "../image/no_photo.png",
-//   ],
-// ]);
-
-// array.push([
-//   "https://site.com/image-2.jpeg",
-//   "Title 2",
-//   "720px",
-//   "1280px",
-// ]);
-
-// array.push([
-//   "https://site.com/image-3.jpeg",
-//   "Title 3",
-//   "1080px",
-//   "1920px",
-// ]);
-
-// // Output imageArray to the console
-// console.log(array);
-
-// ------------TEST ARRAY 2---------------
+// --------- array -------------
 let imageArray = [];
 let name = ('');
 let person0 = {};
@@ -52,24 +20,21 @@ imageArray.push(person0, person1);
 
 console.log(imageArray);
 
-//-------------------TEST ARRAY 3-----------------
+// -------------- array 2 ------------------
 // const people = [
-//   {name: 'Julia Abarca Chavez', image: ["../image/no_photo.png","../image/no_photo.png","../image/no_photo.png",]}, 
-//   {name: 'Andrew', age: 3}, 
-//   {name: 'Peter', age: 8}, 
-//   {name: 'Hanna', age: 14}, 
-//   {name: 'Adam', age: 37}
+//   {
+//     name: "Julia Abarca Chavez",
+//     image: [
+//       "../image/no_photo.png",
+//       "../image/no_photo.png",
+//       "../image/no_photo.png",
+//       "../image/no_photo.png",
+//     ],
+//   },
+//   { name: "Andrew", age: 3 },
+//   { name: "Peter", age: 8 },
+//   { name: "Hanna", age: 14 },
+//   { name: "Adam", age: 37 },
 // ];
-// console.log(people)
-// ------------ append body code ------------------
-// ------------- v1 code ----------------
-var img = document.createElement("img")
-img.src = ("../image/no_photo.png");
-var div = document.getElementById("gallery");
-div.appendChild(img)
 
-// var createResults = function (data){
-//   for (let i = 0; i < person0.image.length; i++){
-//     let
-//   }
-// }
+// console.log(people);
