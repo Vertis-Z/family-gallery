08c1da2af6c954eea892d479ab23e16f0f89d2fe
Michael Zelaya <Anhumimar@gmail.com>
2022-07-22 23:38:29 -0400

    added names to array

--- assets/js/array.js
+++ assets/js/array.js
@@ -1,40 +1,511 @@
+let noImage = "../image/no_photo.png";
+
 // --------- array -------------
 let imageArray = [];
-let name = ('');
-let person0 = {};
+let name = "";
 
-person0.name = "Julia Abarca Chavez";
-person0.profile = "../image/no_photo.png";
-person0.image = ([
-  "../image/no_photo.png",
-  "../image/no_photo.png",
-  "../image/no_photo.png",
-  "../image/no_photo.png",
-]);
+let person000 = {};
+person000.name = "Jesus Mendoza Oseguera";
+person000.profile = noImage;
+person000.image = [noImage, noImage];
 
-let person1 = {};
+let person001 = {};
+person001.name = "Virginia Diaz Flores";
+person001.profile = noImage;
+person001.image = [noImage, noImage];
 
-person1.name = "michael"
+let person002 = {}; 
+person002.name = "Luis Mendoza Diaz"
+person002.profile = noImage;
+person2.image = [noImage, noImage];
+
+let person003 = {}; 
+person003.name = "Esperanza Mendoza Diaz"
+person3.profile = noImage;
+person3.image = [noImage, noImage];
+
+let person004 = {}; 
+person004.name = "Juana Mendoza Diaz"
+person004.profile = noImage;
+person004.image = [noImage, noImage];
+
+let person005 = {}; 
+person005.name = "Maximiliano Mendoza Diaz"
+person005.profile = noImage;
+person005.image = [noImage, noImage];
+
+let person006 = {}; 
+person006.name = "Soledad Mendoza Diaz"
+person006.profile = noImage;
+person006.image = [noImage, noImage];
+
+let person007 = {}; 
+person007.name = "Jesusita Mendoza Diaz"
+person007.profile = noImage;
+person007.image = [noImage, noImage];
+
+let person008 = {}; 
+person008.name = "Susana Mendoza Diaz"
+person008.profile = noImage;
+person008.image = [noImage, noImage];
+
+let person009 = {}; 
+person009.name = "Aurelia Mendoza Diaz"
+person009.profile = noImage;
+person009.image = [noImage, noImage];
+
+let person010 = {}; 
+person010.name = "Francisco Mendoza Diaz"
+person010.profile = noImage;
+person010.image = [noImage, noImage];
+
+let person011 = {}; 
+person011.name = "Alfonso Mendoza Diaz"
+person011.profile = noImage;
+person011.image = [noImage, noImage];
+
+let person012 = {}; 
+person012.name = "Rafael Abarca Figueroa"
+person012.profile = noImage;
+person012.image = [noImage, noImage];
+
+let person013 = {}; 
+person013.name = "Margarita Chavez Mendoza"
+person013.profile = noImage;
+person013.image = [noImage, noImage];
+
+let person014 = {}; 
+person014.name = "Maria Abarca Chavez"
+person014.profile = noImage;
+person014.image = [noImage, noImage];
+
+let person015 = {}; 
+person015.name = "Jose Abarca Chavez"
+person015.profile = noImage;
+person015.image = [noImage, noImage];
+
+let person016 = {}; 
+person016.name = "Rafael Abarca Chavez"
+person016.profile = noImage;
+person016.image = [noImage, noImage];
+
+let person017 = {}; 
+person017.name = "Margarita Abarca Chavez"
+person017.profile = noImage;
+person017.image = [noImage, noImage];
+
+let person018 = {}; 
+person018.name = "Lupe Abarca Chavez"
+person018.profile = noImage;
+person018.image = [noImage, noImage];
+
+let person019 = {}; 
+person019.name = "Julia Abarca Chavez"
+person019.profile = noImage;
+person019.image = [noImage, noImage];
+
+let person020 = {}; 
+person020.name = "Margarita Mendoza Abarca"
+person020.profile = noImage;
+person020.image = [noImage, noImage];
+
+let person021 = {}; 
+person021.name = "Francisco Mendoza"
+person021.profile = noImage;
+person021.image = [noImage, noImage];
+
+let person022 = {}; 
+person022.name = "Leticia Mendoza Abarca"
+person022.profile = noImage;
+person022.image = [noImage, noImage];
+
+let person023 = {}; 
+person023.name = "Lidia Mendoza Abarca"
+person023.profile = noImage;
+person023.image = [noImage, noImage];
+
+let person024 = {}; 
+person024.name = "Rigoberto Mendoza Abarca"
+person024.profile = noImage;
+person024.image = [noImage, noImage];
+
+let person025 = {}; 
+person025.name = "Alfonso Mendoza Abarca"
+person025.profile = noImage;
+person025.image = [noImage, noImage];
+
+let person026 = {}; 
+person026.name = "Jose Luis Manuel Mendoza Abarca"
+person026.profile = noImage;
+person026.image = [noImage, noImage];
+
+let person027 = {}; 
+person027.name = "Maria Angelica Mendoza Abarca"
+person027.profile = noImage;
+person027.image = [noImage, noImage];
+
+let person028 = {}; 
+person028.name = "Blanca Mendoza Abarca"
+person028.profile = noImage;
+person028.image = [noImage, noImage];
+
+let person029 = {}; 
+person029.name = "Lourdes Mendoza Abarca"
+person029.profile = noImage;
+person029.image = [noImage, noImage];
+
+let person030 = {}; 
+person030.name = "Manuel Mendoza Abarca"
+person030.profile = noImage;
+person030.image = [noImage, noImage];
+
+let person031 = {}; 
+person031.name = "Ruben Garcia"
+person031.profile = noImage;
+person031.image = [noImage, noImage];
+
+let person032 = {}; 
+person032.name = "Julie Garcia Mendoza"
+person032.profile = noImage;
+person032.image = [noImage, noImage];
+
+let person033 = {}; 
+person033.name = "Alejandro Garcia Mendoza"
+person033.profile = noImage;
+person033.image = [noImage, noImage];
+
+let person034 = {}; 
+person034.name = "Carmela Guiterrez"
+person034.profile = noImage;
+person034.image = [noImage, noImage];
+
+let person035 = {}; 
+person035.name = "Susana Mendoza Guiterrez"
+person035.profile = noImage;
+person035.image = [noImage, noImage];
+
+let person036 = {}; 
+person036.name = "Francisco Jr. Mendoza Guiterrez"
+person036.profile = noImage;
+person036.image = [noImage, noImage];
+
+let person037 = {}; 
+person037.name = "Rafael Oseguera"
+person037.profile = noImage;
+person037.image = [noImage, noImage];
+
+let person038 = {}; 
+person038.name = "Gerardo Oseguera Mendoza"
+person038.profile = noImage;
+person038.image = [noImage, noImage];
+
+let person039 = {}; 
+person039.name = "Ricardo Oseguera Mendoza"
+person039.profile = noImage;
+person039.image = [noImage, noImage];
+
+let person040 = {}; 
+person040.name = "Esmeralda Oseguera Mendoza"
+person040.profile = noImage;
+person040.image = [noImage, noImage];
+
+let person041 = {}; 
+person041.name = "Jorge Oseguera Mendoza"
+person041.profile = noImage;
+person041.image = [noImage, noImage];
+
+let person042 = {}; 
+person042.name = "Eduardo Silva"
+person042.profile = noImage;
+person042.image = [noImage, noImage];
+
+let person043 = {}; 
+person043.name = "Daisy Silva Mendoza"
+person043.profile = noImage;
+person043.image = [noImage, noImage];
+
+let person044 = {}; 
+person044.name = "Eduardo Silva Mendoza"
+person044.profile = noImage;
+person044.image = [noImage, noImage];
+
+let person045 = {}; 
+person045.name = "Elvia Mendoza Magallon"
+person045.profile = noImage;
+person045.image = [noImage, noImage];
+
+let person046 = {}; 
+person046.name = "Julie Mendoza Mendoza"
+person046.profile = noImage;
+person046.image = [noImage, noImage];
+
+let person047 = {}; 
+person047.name = "Johnny Mendoza Mendoza"
+person047.profile = noImage;
+person047.image = [noImage, noImage];
+
+let person048 = {}; 
+person048.name = "Silvia Lopez"
+person048.profile = noImage;
+person048.image = [noImage, noImage];
+
+let person049 = {}; 
+person049.name = "Cindy Mendoza Lopez"
+person049.profile = noImage;
+person049.image = [noImage, noImage];
+
+let person050 = {}; 
+person050.name = "Alfonso Mendoza Lopez"
+person050.profile = noImage;
+person050.image = [noImage, noImage];
+
+let person051 = {}; 
+person051.name = "Erenia Mendoza Lopez"
+person051.profile = noImage;
+person051.image = [noImage, noImage];
+
+let person052 = {}; 
+person052.name = "Yeraldi Mendoza Lopez"
+person052.profile = noImage;
+person052.image = [noImage, noImage];
+
+let person053 = {}; 
+person053.name = "Itati Mendoza Lopez"
+person053.profile = noImage;
+person053.image = [noImage, noImage];
+
+let person054 = {}; 
+person054.name = "Emeline Mendoza Lopez"
+person054.profile = noImage;
+person054.image = [noImage, noImage];
+
+let person055 = {}; 
+person055.name = "Kevin Mendoza Lopez"
+person055.profile = noImage;
+person055.image = [noImage, noImage];
+
+let person056 = {}; 
+person056.name = "Allen Mendoza Lopez"
+person056.profile = noImage;
+person056.image = [noImage, noImage];
+
+let person057 = {}; 
+person057.name = "Name Here"
+person057.profile = noImage;
+person057.image = [noImage, noImage];
+
+let person058 = {}; 
+person058.name = "Maximiliano Thomas Mendoza Martinez"
+person058.profile = noImage;
+person058.image = [noImage, noImage];
+
+let person059 = {}; 
+person059.name = "Francisco Christian Mendoza Martinez"
+person059.profile = noImage;
+person059.image = [noImage, noImage];
+
+let person060 = {}; 
+person060.name = "Marie Heribertha Mendoza Martinez"
+person060.profile = noImage;
+person060.image = [noImage, noImage];
+
+let person061 = {}; 
+person061.name = "Diego Manuel Mendoza Martinez"
+person061.profile = noImage;
+person061.image = [noImage, noImage];
+
+let person062 = {}; 
+person062.name = "Hugo Zelaya"
+person062.profile = noImage;
+person062.image = [noImage, noImage];
+
+let person063 = {}; 
+person063.name = "Hugo Alexander Zelaya Mendoza"
+person063.profile = noImage;
+person063.image = [noImage, noImage];
+
+let person064 = {}; 
+person064.name = "Michael Jonatan Zelaya Mendoza"
+person064.profile = noImage;
+person064.image = [noImage, noImage];
+
+let person065 = {}; 
+person065.name = "Marvin Zelaya Mendoza"
+person065.profile = noImage;
+person065.image = [noImage, noImage];
+
+let person066 = {}; 
+person066.name = "Angelica Maria Zelaya Mendoza"
+person066.profile = noImage;
+person066.image = [noImage, noImage];
+
+let person067 = {}; 
+person067.name = "Isidro Chavez"
+person067.profile = noImage;
+person067.image = [noImage, noImage];
+
+let person068 = {}; 
+person068.name = "Anthony Chavez Mendoza"
+person068.profile = noImage;
+person068.image = [noImage, noImage];
+
+let person069 = {}; 
+person069.name = "Ashley Chavez Mendoza"
+person069.profile = noImage;
+person069.image = [noImage, noImage];
+
+let person070 = {}; 
+person070.name = "Kimberly Chavez Mendoza"
+person070.profile = noImage;
+person070.image = [noImage, noImage];
+
+let person071 = {}; 
+person071.name = "Kaylyn Chavez Mendoza"
+person071.profile = noImage;
+person071.image = [noImage, noImage];
+
+let person072 = {}; 
+person072.name = "Name Here"
+person072.profile = noImage;
+person072.image = [noImage, noImage];
+
+let person073 = {}; 
+person073.name = "Leslie Ramos Mendoza"
+person073.profile = noImage;
+person073.image = [noImage, noImage];
+
+let person074 = {}; 
+person074.name = "Angel Ramos Mendoza"
+person074.profile = noImage;
+person074.image = [noImage, noImage];
+
+// let person075 = {}; 
+// person075.name = "Name Here"
+// person075.profile = noImage;
+// person075.image = [noImage, noImage];
+
+// let person076 = {}; 
+// person076.name = "Name Here"
+// person076.profile = noImage;
+// person076.image = [noImage, noImage];
+
+// let person077 = {}; 
+// person077.name = "Name Here"
+// person077.profile = noImage;
+// person077.image = [noImage, noImage];
+
+// let person078 = {}; 
+// person078.name = "Name Here"
+// person078.profile = noImage;
+// person078.image = [noImage, noImage];
+
+// let person079 = {}; 
+// person079.name = "Name Here"
+// person079.profile = noImage;
+// person079.image = [noImage, noImage];
+
+// let person080 = {}; 
+// person080.name = "Name Here"
+// person080.profile = noImage;
+// person080.image = [noImage, noImage];
+
+// let person081 = {}; 
+// person081.name = "Name Here"
+// person081.profile = noImage;
+// person081.image = [noImage, noImage];
+
+// let person082 = {}; 
+// person082.name = "Name Here"
+// person082.profile = noImage;
+// person082.image = [noImage, noImage];
+
+// let person083 = {}; 
+// person083.name = "Name Here"
+// person083.profile = noImage;
+// person083.image = [noImage, noImage];
+
+// let person084 = {}; 
+// person084.name = "Name Here"
+// person084.profile = noImage;
+// person084.image = [noImage, noImage];
+
+// let person085 = {}; 
+// person085.name = "Name Here"
+// person085.profile = noImage;
+// person085.image = [noImage, noImage];
+
+// let person086 = {}; 
+// person086.name = "Name Here"
+// person086.profile = noImage;
+// person086.image = [noImage, noImage];
+
+// let person087 = {}; 
+// person087.name = "Name Here"
+// person087.profile = noImage;
+// person087.image = [noImage, noImage];
+
+// let person088 = {}; 
+// person088.name = "Name Here"
+// person088.profile = noImage;
+// person088.image = [noImage, noImage];
+
+// let person089 = {}; 
+// person089.name = "Name Here"
+// person089.profile = noImage;
+// person089.image = [noImage, noImage];
+
+// let person090 = {}; 
+// person090.name = "Name Here"
+// person090.profile = noImage;
+// person090.image = [noImage, noImage];
+
+// let person091 = {}; 
+// person091.name = "Name Here"
+// person091.profile = noImage;
+// person091.image = [noImage, noImage];
+
+// let person092 = {}; 
+// person092.name = "Name Here"
+// person092.profile = noImage;
+// person092.image = [noImage, noImage];
+
+// let person093 = {}; 
+// person093.name = "Name Here"
+// person093.profile = noImage;
+// person093.image = [noImage, noImage];
+
+// let person094 = {}; 
+// person094.name = "Name Here"
+// person094.profile = noImage;
+// person094.image = [noImage, noImage];
+
+// let person095 = {}; 
+// person095.name = "Name Here"
+// person095.profile = noImage;
+// person095.image = [noImage, noImage];
+
+// let person096 = {}; 
+// person096.name = "Name Here"
+// person096.profile = noImage;
+// person096.image = [noImage, noImage];
+
+// let person097 = {}; 
+// person097.name = "Name Here"
+// person097.profile = noImage;
+// person097.image = [noImage, noImage];
+
+// let person098 = {}; 
+// person098.name = "Name Here"
+// person098.profile = noImage;
+// person098.image = [noImage, noImage];
+
+// let person099 = {}; 
+// person099.name = "Name Here"
+// person099.profile = noImage;
+// person099.image = [noImage, noImage];
+
 
 imageArray.push(person0, person1);
 
 console.log(imageArray);
-
-// -------------- array 2 ------------------
-// const people = [
-//   {
-//     name: "Julia Abarca Chavez",
-//     image: [
-//       "../image/no_photo.png",
-//       "../image/no_photo.png",
-//       "../image/no_photo.png",
-//       "../image/no_photo.png",
-//     ],
-//   },
-//   { name: "Andrew", age: 3 },
-//   { name: "Peter", age: 8 },
-//   { name: "Hanna", age: 14 },
-//   { name: "Adam", age: 37 },
-// ];
-
-// console.log(people);
+person0.children = [person0, person1];
